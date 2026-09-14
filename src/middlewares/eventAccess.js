ffa12d803bcbf7d0631ca7c846aceff6f94e8446
bigoluwagentle <olayiwolaolabode06@gmail.com>
2026-09-14 00:41:19 -0700

    updated

--- src/middlewares/eventAccess.js
+++ src/middlewares/eventAccess.js
@@ -21,4 +21,21 @@ function requireEventAccess(requiredPermission) {
   });
 }
 
-module.exports = { requireEventAccess };
+/**
+ * Looser check: any org member (owner/admin/manager) OR any EventStaff record
+ * at all, regardless of specific permission. Use for read-only views (like
+ * viewing the schedule) where every staff role should reasonably be able to see it.
+ */
+const requireAnyEventAccess = asyncHandler(async (req, res, next) => {
+  if (req.membership) {
+    return next();
+  }
+  const staffRecord = await EventStaff.findOne({ event: req.event._id, user: req.user._id });
+  if (!staffRecord) {
+    return next(new AppError('You do not have access to this event', 403));
+  }
+  req.staffPermissions = staffRecord.permissions;
+  next();
+});
+
+module.exports = { requireEventAccess, requireAnyEventAccess };
